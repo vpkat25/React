09e40a70a7ffaea487f785678a844f9e5e241ff8
Vivek <vivekspatil25@gmail.com>
2026-01-31 11:48:54 +0530

    Episode 3 : Componenet composition

--- app.js
+++ app.js
@@ -1,43 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// React.createElement => Object => After rendering => HTMLElement(render)
 
-// const Title = () =>  (
-//     <h1 className="head" tabIndex="5">
-//         Title
-//     </h1>
-// );
-
-// // Componenet Composistion
-// const HeadingComponent = () => (
-//     <div id="container">
-//         <Title />
-//         <h1 className="heading">Namaste React using JSX</h1>
-//     </div>
-// );
-
-const number = 1000;
-
-const elem = <span> React Element</span>
-
-
-const Title =  (
+const Title = () =>  (
     <h1 className="head" tabIndex="5">
-        Title {elem}
+        Title
     </h1>
 );
 
 
+
 const HeadingComponent = () => (
     <div id="container">
-        {Title}
+        <Title />
         <h1 className="heading">Namaste React using JSX</h1>
     </div>
 );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-// root.render(jsxHeading);
 root.render(<HeadingComponent />)
 
